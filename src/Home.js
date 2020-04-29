e2adabbd3cd3eb1dfdceb7e392b1b8a79fad8475
Michael Lund <45572620+hackoverride@users.noreply.github.com>
2020-04-29 18:51:20 +0200

    Continued working on index.js and home.js home.css
    Removed animation, added more delicate colors

--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,13 @@
 import React from "react";
 import Logo from "./lundlogo.svg";
+import "./home.css";
 
 function Home() {
     return (
-
         <div id="homeholder">
             <img src={Logo} alt="Logo" width="400" id="mainlogo"></img>
-            <h1>Michael W. A. Lund</h1>
+            <div className="tittelholder"><h1 className="overskrifter">Michael W. A. Lund</h1><span>Student | Programmerer | Pappa | DM</span></div>
         </div>
-
     );
 }
 
